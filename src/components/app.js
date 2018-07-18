279973ba656aad34eb891161af8c09c8535b3417
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-18 11:05:40 -0600

    added first skews

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Countdown</h1>
+      <div className="grid">
+        <div>Countdown</div>
+        <div>Hello</div>
+        <div>Hi</div>
+        <div className="grid__skew-light-one"></div>
+        <div className="grid__skew-light-two"></div>
+        <div className="grid__skew-light-three-box"></div>
       </div>
     );
   }
