c455c6d9f4ba2ec9b960c65218c32e6b5e245743
Jared Grant <jared.grant@canyonsdistrict.org>
2018-08-08 13:40:50 -0600

    fixed remaining bugs

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+
 import Picker from './picker';
 import Button from './button';
 import Clock from './clock';
 import ChangeDate from './changeDate';
 import LargeText from './largeText';
-import moment from 'moment';
+
+import moment from "moment";
 
 export default class App extends Component {
-
   constructor(props) {
     super(props)
 
@@ -25,26 +26,28 @@ export default class App extends Component {
       age: 0
     };
 
-    this.handleGenerate = this.handleGenerate.bind(this)
+    this.handleGenerate = this.handleGenerate.bind(this);
   }
 
   handleChange = function(date) {
     console.log('APP JS HANDLE CHANGE', date._d);
-    clearInterval(this.timer);
+  
     this.setState({
         startDate: date
     });
-  }.bind(this)
+  }.bind(this);
 
   handleGenerate = function() {
+    clearInterval(this.timer)
 
     var bday = this.state.startDate.toDate();
     var today = new Date();
     var currentMonth = today.getMonth();
     var birthMonth = bday.getMonth();
+
     var timeBetween = today.getTime() - bday.getTime();
-    var daysOld = Math.floor(timeBetween / (1000 * 60 * 60 * 24));
-    var yearsOld = Number((daysOld/365).toFixed(0));
+    var daysOld = Math.floor(timeBetween / (1000 * 60 * 60 * 24))
+    var age = Number((daysOld/365).toFixed(0));
     this.setState({ 
       age, 
       active: true 
@@ -52,9 +55,9 @@ export default class App extends Component {
 
     if(birthMonth > currentMonth) {
       bday.setFullYear(today.getFullYear())
-    } else if (birthMonth < currentMonth) {
+    } else if(birthMonth < currentMonth) {
       bday.setFullYear(today.getFullYear() + 1)
-    } else if (birthMonth == currentMonth) {
+    } else if(birthMonth == currentMonth) {
       var currentDay = today.getDate();
       var birthDay = bday.getDate();
 
@@ -71,13 +74,15 @@ export default class App extends Component {
     this.timer = setInterval(function() {
       
       var now = moment().toDate().getTime();
-      
       var distance = countDownDate - now;
 
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / (1000));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
       const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
       const timeRemaining = {
@@ -85,52 +90,52 @@ export default class App extends Component {
         hours,
         minutes,
         seconds
-      }
+      };
       
       this.setState({ timeRemaining });
 
       if (distance < 0) {
         clearInterval(this.timer);
-        //document.getElementById("demo").innerHTML = "EXPIRED";
+        // document.getElementById("demo").innerHTML = "EXPIRED";
       }
     }.bind(this), 1000);
   }.bind(this);
 
-  getBirthDate = function() {
+  getBirthDate = function(date) {
     const month = date.getMonth() + 1;
     const day = date.getDate();
     if(month < 10) {
-      return '0${month}/${day}'
+      return `0${month}/${day}`
     }
-    return '${month}/${day}'
+    return `${month}/${day}`
   }.bind(this);
 
   renderItems = function() {
     if(this.state.active) {
       return [
-        <Clock key={0} timeRemaining={this.state.timeRemaining}/>,
+        <Clock key={0} timeRemaining={this.state.timeRemaining} />,
         ChangeDate('Change Date', () => this.setState({ active: false })),
         LargeText(this.getBirthDate(this.state.startDate.toDate())),
         <label key={3} className="grid__remaining">
         Remaining until you turn {this.state.age}
         </label>
-      ]
+      ];
     } else {
       return [
         <Picker 
         startDate={this.state.startDate} 
-        callback={(date) => this.handleChange(date)}
+        callback={date => this.handleChange(date)}
         key={0}
         />,
-        Button('Generate Countdown', () => this.setState({ active: true }))
-      ] 
+        Button('Generate Countdown', () => this.handleGenerate())
+      ]; 
     }
-  }.bind(this)
+  }.bind(this);
 
   render() {
     return (
       <div className="grid">
-        <h1 className="grid__title">Countdown</h1>
+        <h1 className="grid__title">Birthday Countdown</h1>
 
         {/*<div className="grid__skew-dark-one-box"></div>*/}
         <div className="grid__skew-dark-two"></div>
@@ -140,9 +145,10 @@ export default class App extends Component {
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
 
-        { this.renderItems() }
-
+        {this.renderItems()}
       </div>
     );
   }
 }
+
+// https://github.com/bottega-code-school/BirthdayCountdownApp
